1698cadf7203edce5ebfbb9871eff88cb97421ef
Kamil Kostrzewski <kamil.kostrzewski95@gmail.com>
2019-04-20 20:35:46 +0200

    Add Book DbContext

---
+++ src/DotNETCoreWebAPI/DbContexts/BookContext.cs
@@ -0,0 +1,12 @@
+﻿using DotNETCoreWebAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DotNETCoreWebAPI.DbContexts
+{
+    public class BookContext: DbContext
+    {
+        public BookContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<Book> Measurements { get; set; }
+    }
+}
