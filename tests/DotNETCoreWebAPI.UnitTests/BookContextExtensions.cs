9c71fad1363af5e3c2fd121b1a5b4b54c2504f3c
Kamil Kostrzewski <kamil.kostrzewski95@gmail.com>
2019-04-20 22:50:49 +0200

    Add book mocker with extensions

---
+++ tests/DotNETCoreWebAPI.UnitTests/BookContextExtensions.cs
@@ -0,0 +1,73 @@
+﻿using DotNETCoreWebAPI.DbContexts;
+using DotNETCoreWebAPI.Models;
+
+namespace DotNETCoreWebAPI.UnitTests
+{
+    public static class BookContextExtensions
+    {
+        public static void FillDatabase(this BookContext dbContext)
+        {
+            dbContext.Books.Add
+            (
+                new Book
+                {
+                    Id = 1,
+                    Title = "Book_01",
+                    Author = "Author_01",
+                    ISBN = "0228260269",
+                    Year = 2000
+                }
+            );
+
+            dbContext.Add
+            (
+                new Book
+                {
+                    Id = 2,
+                    Title = "Book_02",
+                    Author = "Author_02",
+                    ISBN = "0618233269",
+                    Year = 2001
+                }
+            );
+
+            dbContext.Add
+            (
+                new Book
+                {
+                    Id = 3,
+                    Title = "Book_03",
+                    Author = "Author_03",
+                    ISBN = "0618264469",
+                    Year = 2002
+                }
+            );
+
+            dbContext.Add
+            (
+                new Book
+                {
+                    Id = 4,
+                    Title = "Book_04",
+                    Author = "Author_04",
+                    ISBN = "0618260233",
+                    Year = 2000
+                }
+            );
+
+            dbContext.Add
+            (
+                new Book
+                {
+                    Id = 5,
+                    Title = "Book_05",
+                    Author = "Author_05",
+                    ISBN = "0618260559",
+                    Year = 2010
+                }
+            );
+
+            dbContext.SaveChanges();
+        }
+    }
+}
---
+++ tests/DotNETCoreWebAPI.UnitTests/BookContextMocker.cs
@@ -0,0 +1,25 @@
+﻿using DotNETCoreWebAPI.DbContexts;
+using DotNETCoreWebAPI.Models;
+using DotNETCoreWebAPI.Repositories;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DotNETCoreWebAPI.UnitTests
+{
+    public static class BookContextMocker
+    {
+        public static IBookRepository<Book> GetInMemoryRepository(string dbName)
+        {
+            var options = new DbContextOptionsBuilder<BookContext>()
+                .UseInMemoryDatabase(databaseName: dbName)
+                .Options;
+
+            BookContext bookContext = new BookContext(options);
+            bookContext.FillDatabase();
+
+            return new BookRepository(bookContext);
+        }
+    }
+}
