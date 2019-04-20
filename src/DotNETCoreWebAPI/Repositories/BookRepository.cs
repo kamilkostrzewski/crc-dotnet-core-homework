05e1ed5bfb260a1d0e1b9578de640dbbd1625780
Kamil Kostrzewski <kamil.kostrzewski95@gmail.com>
2019-04-20 22:08:41 +0200

    Add BookRepository

---
+++ src/DotNETCoreWebAPI/Repositories/BookRepository.cs
@@ -0,0 +1,50 @@
+﻿using DotNETCoreWebAPI.DbContexts;
+using DotNETCoreWebAPI.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace DotNETCoreWebAPI.Repositories
+{
+    public class BookRepository
+    {
+        private readonly BookContext _dbContext;
+
+        public BookRepository(BookContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<IEnumerable<Book>> GetAll()
+        {
+            return await _dbContext.Books.ToListAsync();
+        }
+
+        public async Task<Book> Get(long id)
+        {
+            return await _dbContext.Books.FirstOrDefaultAsync(b => b.Id == id);
+        }
+
+        public async Task Add(Book entity)
+        {
+            await _dbContext.Books.AddAsync(entity);
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task Update(Book book, Book entity)
+        {
+            book.Title = entity.Title;
+            book.Author = entity.Author;
+            book.ISBN = entity.ISBN;
+            book.Year = entity.Year;
+
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task Delete(Book book)
+        {
+            _dbContext.Books.Remove(book);
+            await _dbContext.SaveChangesAsync();
+        }
+    }
+}
---
+++ src/DotNETCoreWebAPI/Repositories/IBookRepository.cs
@@ -0,0 +1,15 @@
+﻿using DotNETCoreWebAPI.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace DotNETCoreWebAPI.Repositories
+{
+    public interface IBookRepository<TEntity>
+    {
+        Task<IEnumerable<TEntity>> GetAll();
+        Task<TEntity> Get(long id);
+        Task Add(TEntity entity);
+        Task Update(Book book, TEntity entity);
+        Task Delete(Book book);
+    }
+}
