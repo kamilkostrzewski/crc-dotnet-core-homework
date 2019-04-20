8bc9a7f8c525c62a7d778c34268aaecfe343245e
Kamil Kostrzewski <kamil.kostrzewski95@gmail.com>
2019-04-20 21:57:35 +0200

    Add seed migration

---
+++ src/DotNETCoreWebAPI/Migrations/20190420194422_FillBooksTable.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DotNETCoreWebAPI.Migrations
+{
+    public partial class FillBooksTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(
+                @"
+                INSERT INTO Books (
+                      Id,
+                      Title,
+                      Author,
+                      ISBN,
+                      Year
+                  )
+                  VALUES (
+                      '1',
+                      ' The Lord of the Rings: The Fellowship of the Ring',
+                      'J. R. R. Tolkien',
+                      '0618260269',
+                      '2003'
+                  );"
+            );
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(
+                @"DELETE FROM Books WHERE Id = '1'"
+            );
+        }
+    }
+}
